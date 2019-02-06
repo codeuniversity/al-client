c67e0562fd10f55c3d6fd3e4c63cef635f32e327
Elias Wolf <Elias-Wolf@t-online.de>
2019-02-06 14:21:33 +0100

    Add Simple Canvas prototype

--- main.js
+++ main.js
@@ -1,14 +1,36 @@
-const socket = new WebSocket('ws://localhost:4000');
+var canvas = document.getElementById("canvas3");
+canvas.width = window.innerWidth;
+canvas.width = window.innerWidth;
+console.log(window.innerHeight);
+console.log(window.innerWidth);
+canvas.height = window.innerHeight;
+var ctx = canvas.getContext("2d");
+
+window.onresize = function (){
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+
+var socket = new WebSocket('ws://localhost:4000');
+
+function draw_cell(x, y, r, color){
+  ctx.beginPath();
+  ctx.arc(x, y, r, 0, Math.PI * 2, false);
+  ctx.fillStyle = color;
+  ctx.fill();
+  ctx.closePath()
+}
 
 // Connection opened
 socket.addEventListener('open', function (event) {
     console.log("Socket established")
 });
+
 // Listen for messages
 socket.addEventListener('message', function (event) {
-  const data = JSON.parse(event.data)
-  console.log(data)
-  const container = document.getElementById("container");
-  container.innerHTML = JSON.stringify(data)
+  ctx.clearRect(0,0,canvas.width,canvas.height)
+  JSON.parse(event.data).forEach(e => {
+    var pos = e['pos']
+    draw_cell(pos['x'], pos['y'], 5,"white")
+  });
 });
-
